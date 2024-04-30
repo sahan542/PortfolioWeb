2e4f2165696e15ea359ec0578b26e544de44df37
sahan542 <sahanrashmika542@gmail.com>
2024-05-01 03:41:53 +0530

    footer added

--- script.js
+++ script.js
@@ -63,3 +63,19 @@ circles.forEach(elem=>{
 
 //mix it up portfolio section
 var mixer = mixitup('.portfolio-gallery');
+
+//active menu ////////////////////////////////
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){
+
+    }
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+
+}
+activeMenu();
+window.addEventListener("scroll",activeMenu);
